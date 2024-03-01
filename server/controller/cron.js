c483aa67a056149c40926b965f7b7543c633d68c
utkarsh-radiant <utkarsh@radiantinfonet.com>
2024-03-01 17:02:50 +0530

    done

--- server/controller/cron.js
+++ server/controller/cron.js
@@ -1,19 +1,41 @@
 import cron from "node-cron";
+import nodemailer from "nodemailer";
 
-function logMessage() {
-  console.log("Cron job executed at:", new Date().toLocaleString());
-}
 
-function logMessage2() {
-  console.log("One am Cron job executed at:", new Date().toLocaleString());
+async function sendMail() {
+  // Create a transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    auth: {
+      user: "utkarsh.uitest@gmail.com",
+      pass: "jndbcnmrtjhqohzx",
+    },
+  });
+
+  // Send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"AY Ventures" <no-reply@ayventures.in>', // sender address
+    to: "utkarshk495@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
 }
 
 export function startCronJob() {
-  cron.schedule("* * * * *", () => {
-    logMessage();
+  cron.schedule('0 * * * *', async () => {
+    try {
+      await sendMail();
+      console.log('Email sent successfully');
+    } catch (error) {
+      console.error('Error sending email:', error);
+    }
   });
 
   cron.schedule("0 1 * * *", () => {
-    logMessage2();
+    sendMail();
   });
 }
